b5ca7c61ee87b009bcd0793ea7b0afa783b90458
Rahul <pbrkumawat@gmail.com>
2026-06-18 19:21:11 +0530

    Char: Add top destinations

--- config/multer_config.js
+++ config/multer_config.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "/temp");
+    cb(null, "/tmp");
   },
 
   filename: (req, file, cb) => {
