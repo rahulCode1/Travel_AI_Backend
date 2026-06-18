530b2b21f31a48abd22a3f922ca2965add2d363d
Rahul <pbrkumawat@gmail.com>
2026-06-18 19:19:30 +0530

    Char: Add top destinations

--- config/multer_config.js
+++ config/multer_config.js
@@ -1,9 +1,8 @@
 import multer from "multer";
-import path from "path"
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join("uploads"));
+    cb(null, "/temp");
   },
 
   filename: (req, file, cb) => {
